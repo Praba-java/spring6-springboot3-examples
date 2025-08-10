8ebd516fca462bdcdabccb30a4620fbe91daa728
prabakaranv <prabas.info@gmail.com>
2025-08-10 12:40:30 +0530

    Spring XML Config

---
+++ src/main/java/org/example/Desktop.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Desktop implements Computer{
+
+
+    @Override
+    public void getcompile() {
+        System.out.println("Compiling from Desktop");
+    }
+}
---
+++ src/main/java/org/example/Laptop.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Laptop implements Computer{
+
+
+    @Override
+    public void getcompile() {
+        System.out.println("Compiling from Laptop");
+    }
+}
